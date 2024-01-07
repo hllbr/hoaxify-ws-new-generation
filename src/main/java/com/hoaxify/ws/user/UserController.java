5feb5bf2917660f1138dbd0b1fbff59f2a8318e6
hllbr <halibrahim.kocak@gmail.com>
2024-01-07 19:20:19 +0300

    user create operation featured

---
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -0,0 +1,21 @@
+package com.hoaxify.ws.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hoaxify.ws.shared.GenericMessage;
+
+@RestController
+public class UserController {
+
+    @Autowired
+    UserService userService;
+
+    @PostMapping("/api/v1/users")
+    GenericMessage createUser(@RequestBody User user) {
+        userService.save(user);
+        return new GenericMessage("User is created");
+    }
+}
