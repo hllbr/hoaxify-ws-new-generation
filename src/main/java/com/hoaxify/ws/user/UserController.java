0eab1ab0d4eda54fe13c872082a468dd664594c2
hllbr <halibrahim.kocak@gmail.com>
2024-03-09 20:08:50 +0300

    user activate api created and messages handler

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -8,6 +8,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -17,6 +19,7 @@ import com.hoaxify.ws.shared.GenericMessage;
 import com.hoaxify.ws.shared.Messages;
 import com.hoaxify.ws.user.dto.UserCreate;
 import com.hoaxify.ws.user.exception.ActivationNotificationException;
+import com.hoaxify.ws.user.exception.InvalidTokenException;
 import com.hoaxify.ws.user.exception.NotUniqueEmailException;
 
 import jakarta.validation.Valid;
@@ -30,8 +33,18 @@ public class UserController {
         @PostMapping("/api/v1/users")
         GenericMessage createUser(@Valid @RequestBody UserCreate user) {
                 userService.save(user.toUser());
-                String message = Messages.getMessageForLocale("hoaxify.create.user.success.message",
-                                LocaleContextHolder.getLocale());
+                String message = Messages
+                                .getMessageForLocale("hoaxify.create.user.success.message",
+                                                LocaleContextHolder.getLocale());
+                return new GenericMessage(message);
+        }
+
+        @PatchMapping("/api/v1/users/{token}/active")
+        GenericMessage activateUser(@PathVariable String token) {
+                userService.activateUser(token);
+                String message = Messages
+                                .getMessageForLocale("hoaxify.activate.user.success.message",
+                                                LocaleContextHolder.getLocale());
                 return new GenericMessage(message);
         }
 
@@ -66,8 +79,18 @@ public class UserController {
                 ApiError apiError = new ApiError();
                 apiError.setPath("/api/v1/users");
                 apiError.setMessage(exception.getMessage());
-                apiError.setStatus(502);//bad get way
+                apiError.setStatus(502);// bad get way
 
                 return ResponseEntity.status(502).body(apiError);
+        }
+
+        @ExceptionHandler(InvalidTokenException.class)
+        ResponseEntity<ApiError> handleInvalidTokenException(InvalidTokenException exception) {
+                ApiError apiError = new ApiError();
+                apiError.setPath("/api/v1/users");
+                apiError.setMessage(exception.getMessage());
+                apiError.setStatus(400);
+
+                return ResponseEntity.status(400).body(apiError);
         }
 }
--- src/main/java/com/hoaxify/ws/user/dto/UserCreate.java
+++ src/main/java/com/hoaxify/ws/user/dto/UserCreate.java
@@ -9,16 +9,24 @@ import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record UserCreate(
-        @NotBlank(message = "{Hoaxify.Constraint.UserName.NotBlank}") @Size(min = 5, max = 255) String username,
+        @NotBlank(message = "{Hoaxify.Constraint.UserName.NotBlank}") 
+        @Size(min = 5, max = 255) 
+        String username,
 
-        @NotBlank @Email @UniqueEmail String email,
+        @NotBlank 
+        @Email 
+        @UniqueEmail 
+        String email,
 
-        @Size(min = 8, max = 55) @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{Hoaxify.Constraint.Password.Pattern}") String password) {
+        @Size(min = 8, max = 55) 
+        @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d).*$", message = "{Hoaxify.Constraint.Password.Pattern}")
+        String password) 
+        {
 
     public User toUser() {
         User user = new User();
-        user.setUsername(username);
         user.setEmail(email);
+        user.setUsername(username);
         user.setPassword(password);
         return user;
     }
