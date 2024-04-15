e9edef8e4cbccd146231b94deb1b1bb6b04203d3
hllbr <halibrahim.kocak@gmail.com>
2024-04-15 23:35:53 +0300

    login operation featured and user error operations refoctored

---
+++ src/main/java/com/hoaxify/ws/auth/AuthController.java
@@ -0,0 +1,27 @@
+package com.hoaxify.ws.auth;
+
+import org.springframework.web.bind.annotation.RestController;
+
+import com.hoaxify.ws.auth.dto.AuthResponse;
+import com.hoaxify.ws.auth.dto.Credentials;
+
+import jakarta.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+@RestController
+public class AuthController {
+
+    @Autowired
+    AuthService authservice;
+
+    @PostMapping("/api/v1/auth")
+    AuthResponse handleAuthentication(@Valid @RequestBody Credentials creds) {
+        return authservice.authenticate(creds);
+
+    }
+
+}
