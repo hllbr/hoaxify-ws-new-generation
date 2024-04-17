d7779e9600768d2b5ff0f21291b619bd93632716
hllbr <halibrahim.kocak@gmail.com>
2024-04-18 01:36:50 +0300

    Removing the Logged In User from the User List

--- src/main/java/com/hoaxify/ws/user/UserController.java
+++ src/main/java/com/hoaxify/ws/user/UserController.java
@@ -1,33 +1,23 @@
 package com.hoaxify.ws.user;
 
-import java.util.stream.Collectors;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.ResponseEntity;
-import org.springframework.validation.FieldError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.hoaxify.ws.error.ApiError;
+import com.hoaxify.ws.auth.token.TokenService;
 import com.hoaxify.ws.shared.GenericMessage;
 import com.hoaxify.ws.shared.Messages;
 import com.hoaxify.ws.user.dto.UserCreate;
 import com.hoaxify.ws.user.dto.UserDTO;
-import com.hoaxify.ws.user.exception.ActivationNotificationException;
-import com.hoaxify.ws.user.exception.InvalidTokenException;
-import com.hoaxify.ws.user.exception.NotFoundException;
-import com.hoaxify.ws.user.exception.NotUniqueEmailException;
 
-import jakarta.servlet.http.HttpServletRequest;
 import jakarta.validation.Valid;
 
 @RestController
@@ -35,6 +25,9 @@ public class UserController {
 
         @Autowired
         UserService userService;
+
+        @Autowired
+        TokenService tokenService;
 
         @PostMapping("/api/v1/users")
         GenericMessage createUser(@Valid @RequestBody UserCreate user) {
@@ -57,8 +50,10 @@ public class UserController {
         }
 
         @GetMapping("/api/v1/users")
-        Page<UserDTO> getUsers(Pageable page) {
-                return userService.getUsers(page).map(UserDTO::new);
+        Page<UserDTO> getUsers(Pageable page,
+                        @RequestHeader(name = "Authorization", required = false) String authorizationHeader) {
+                var loggedInUser = tokenService.verifyToken(authorizationHeader);
+                return userService.getUsers(page, loggedInUser).map(UserDTO::new);
         }
 
         @GetMapping("api/v1/users/{id}")
